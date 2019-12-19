e9c9e55465e9d6e959a28f646bafee07dd0ce28d
samiksha <samiksha9914@gmail.com>
2019-12-20 01:26:39 +0530

    My first Flutter App

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,31 @@
 import "package:flutter/material.dart";
 
-void main()
-{
-  runApp(
-    Center(child:
-      Text("Hello Flutter",
-        textDirection: TextDirection.ltr)
-    )
-  );
+void main()=> runApp(new Flutter_App());
+
+
+class Flutter_App extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title:" My flutter App",
+        home:Scaffold(
+            appBar: AppBar(title:Text("My First Flutter Application"),),
+            body:Material(
+                color:Colors.lightBlueAccent,
+                child:Center(
+                    child:Text(
+                        "Hello Flutter",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(color:Colors.black,fontSize: 40.0)
+                    )
+                )
+            )
+
+        )
+    );
+
+    return null;
+  }
+
 }
